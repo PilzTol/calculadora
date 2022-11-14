de44f3ecaf4b8240ad1db961f291df642d604e86
Wallisson <wallissonvictor16@gmail.com>
2022-11-14 20:28:56 -0300

    First Commit

---
+++ R/somar.R
@@ -0,0 +1,19 @@
+#' Função para somar
+#'
+#' Esta função serve para somar dois números
+#'
+#' @param a um número
+#' @param b Outro número
+#'
+#' @examples
+#' somar(3,4)
+#'
+#' @export
+somar <- function(a, b){
+
+  a + b
+}
+#' @export
+subtrair <- function(a, b){
+  a - b
+}
